d9d2ab365c42d2bca59c23fcb42bc010bd846927
Simon Pickartz <spickartz@eonerc.rwth-aachen.de>
2015-08-18 10:03:35 +0200

    add pscom_trylock()

--- lib/pscom/pscom_priv.h
+++ lib/pscom/pscom_priv.h
@@ -492,6 +492,14 @@ void pscom_lock(void)
 	assert(res_mutex_lock == 0);
 }
 
+static inline
+int pscom_trylock(void)
+{
+	/* TODO: Is -1 the correct choice? */
+	if (!pscom.threaded) return -1;
+	int res_mutex_lock;
+	return pthread_mutex_lock(&pscom.global_lock);
+}
 
 void pscom_unlock(void);
 
